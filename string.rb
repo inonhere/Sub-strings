2cf9a04ca0878c3942a4885306616257de0cecea
Inon <pokemonxdloquendo@gmail.com>
2022-07-06 11:08:07 -0400

    ported to method

--- string.rb
+++ string.rb
@@ -1,25 +1,20 @@
 dictionary = ["below","down","go","going","horn","how","howdy",
 "it","i","low","own","part","partner","sit"]
-palabrin = 'low'
+palabrin = "low"
 arrayOtro = []
 
-
-dictionary.each do |palabra|
-    p x = palabra.split('')
-    z = palabrin.split('')
-    if x == z
-        y = x.join
-        arrayOtro.push(y)
-    elsif palabra.include?(palabrin)
-        arrayOtro.push(palabra)
+def substrings(words, dictio, dictioFinal)  
+    dictio.each do |palabra|
+        dic_cortado = palabra.split('')
+        pal_cortado = words.split('')
+        if dic_cortado == pal_cortado
+            dic_unido = dic_cortado.join
+            dictioFinal.push(dic_unido)
+        elsif palabra.include?(words)
+            dictioFinal.push(palabra)
+        end
     end
 end
 
+substrings(palabrin, dictionary, arrayOtro)
 p arrayOtro
-
-=begin
-a = 'apple'
-b = a.split('')
-c = b[0]
-p c
-=end
