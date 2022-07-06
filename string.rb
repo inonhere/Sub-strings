ed82534ababe46873a208b8155392cf3116e7a1b
Inon <pokemonxdloquendo@gmail.com>
2022-07-06 13:36:35 -0400

    better formatting

--- string.rb
+++ string.rb
@@ -1,24 +1,22 @@
 dictionary = ["below","down","go","going","horn","how","howdy",
     "it","i","low","own","part","partner","sit"]
-    palabrin = "i p"
-    arrayOtro = []
-    
-    def substrings(words, dictio, dictioFinal)  
-        dictio.each do |palabra|
-            dic_cortado = palabra.split('')
-            pal_cortado = words.split(' ')
-            pal_cortado.each do |palabrota|
-               pal_corta = palabrota.split('')
-               pal_corta_unida = pal_corta.join
-                if dic_cortado == pal_corta
-                        dic_unido = dic_cortado.join
-                        dictioFinal.push(dic_unido)
-                elsif palabra.include?(pal_corta_unida)
-                    dictioFinal.push(palabra)
-                end
+palabrin = "low"
+arrayOtro = []
+def substrings(words, dictio, dictioFinal)  
+    dictio.each do |palabra|
+        dic_cortado = palabra.split('')
+        pal_cortado = words.split(' ')
+        pal_cortado.each do |palabrota|
+            pal_corta = palabrota.split('')
+            pal_corta_unida = pal_corta.join
+            if dic_cortado == pal_corta
+                dic_unido = dic_cortado.join
+                dictioFinal.push(dic_unido)
+            elsif palabra.include?(pal_corta_unida)
+                dictioFinal.push(palabra)
             end
         end
     end
-    
-    substrings(palabrin, dictionary, arrayOtro)
-    p arrayOtro
+end
+substrings(palabrin, dictionary, arrayOtro)
+p arrayOtro
